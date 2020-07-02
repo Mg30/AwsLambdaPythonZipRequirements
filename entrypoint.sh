b6839c68e8c81cd79be7c1b2b837a800530c89b3
matt <matgonzalez736@gmail.com>
2020-07-02 16:21:44 +0200

    add configurable name to dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@ cd $DEPENDENCIES_DIR
 python -m venv venv && \
     source venv/bin/activate && \
     pip install -r ./requirements.txt -t python/lib/python3.7/site-packages/
-zip -r dependencies.zip python
+zip -r $PACKAGE_NAME.zip python
